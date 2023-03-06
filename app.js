29756648e3a4fa061626970d70b029d63c8a1455
sammy2342 <sammyfares303@gmail.com>
2023-03-05 22:11:31 -0500

    complete basic design

---
+++ app.js
@@ -0,0 +1,13 @@
+let randomNumber 
+let rock = 0 
+let paper = 1 
+let sicciors = 2
+
+
+
+const getComputerChoice = () => {
+    randomNumber  = Math.floor(Math.random() * 3)
+}
+const playerChoice = () => {
+
+}
